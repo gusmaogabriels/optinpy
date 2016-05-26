ccae5b01c86cacf18f56470fa8dbb1357bd41486
Gabriel S. Gusmão <gusmaogabriels@gmail.com>
2016-05-26 15:11:06 -0300

    Create __init__.py

---
+++ optinpy/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, absolute_import, print_function
+
+__author__ = {'Gabriel S. Gusmao' : 'gusmaogabriels@gmail.com'}
+__version__ = '1.0a'
+
+"""
+By Gabriel S. Gusmão (Gabriel Sabença Gusmão)
+Oct, 2015
+    optinpy version 1.0a
+    ~~~~
+    "General linear and nonlinear optimization methods"
+    :copyright: (c) 2015 Gabriel S. Gusmão
+    :license: GPU, see LICENSE for more details.
+"""
+
+import numpy as np
+from .graph import *
+
+__all__ = ['graph','np','__author__','__version__']
