b814af8f1ca25277e96469a639c9845f4bae9e6d
Gabriel S. Gusmão <gusmaogabriels@gmail.com>
2016-05-27 22:07:00 -0300

    Dijktra's algorithm enhancements
    Added verbose mode (following Prof. Akebo's nomenclatures)

--- optinpy/graph/shortest_paths/shortest_paths.py
+++ optinpy/graph/shortest_paths/shortest_paths.py
@@ -6,7 +6,7 @@ def bfm(network,n0):
     
     return 
 
-def dijkstra(network,n0):
+def dijkstra(network,n0,verbose=False):
     nodes = network.nodes.keys()
     arcs = network.arcs
     ds = [float('inf') for i in range(0,len(nodes))] # distances list
@@ -15,7 +15,9 @@ def dijkstra(network,n0):
     cn = [on.pop(on.index(n0))] # closed nodes list (F)
     ds[nodes.index(n0)] = 0
     r = n0
+    t = 0
     while len(on)>0:
+        r0 = r
         node = network.nodes[r]
         for i in node.children:
             if ds[nodes.index(i)] > network.costs[arcs.index([r,i])] + ds[nodes.index(r)]:
@@ -25,4 +27,14 @@ def dijkstra(network,n0):
                 pass
         r = min(zip([ds[nodes.index(i)] for i in on], on))[1]
         cn += [on.pop(on.index(r))]
+        t += 1
+        if verbose:
+            print('Iteration #{}'.format(t))
+            print('r0: {}'.format(r0))
+            print('d: {}'.format(ds))
+            print('rot: {}'.format(rot))
+            print('F: {}'.format(cn))
+            print('B: {}'.format(on))
+            print('V: {}'.format(node.children))
+            print('r: {}'.format(r))
     return rot, ds
