4be90fb327852ca43c4e30a56593735ffb000c0f
Gabriel S. Gusmão <gusmaogabriels@gmail.com>
2016-05-28 15:18:57 -0300

    Prim's algorithm revisited (again)
    I had made a huge mess when trying to use Dijkstra's algorithm as base
    for Prim's... now the latter it works smoothly.

--- optinpy/graph/mst/mst.py
+++ optinpy/graph/mst/mst.py
@@ -2,7 +2,7 @@
 
 from ..base import argparser, graph 
 
-def prim(network,verbose=False):
+def prim(network,n0,verbose=False):
     '''
         Prims's algorithm
         This algorithm assumes undirected network inputs. If they aren't, it will assume so.
@@ -10,25 +10,28 @@ def prim(network,verbose=False):
         ..verbose as boolean
     '''
     nodes = network.nodes.keys()
-    arcs0 = list(network.arcs)
-    costs0 = list(network.costs)
     on = list(nodes) # open nodes list (O)
-    cn = []
     arcs = []
+    cn = [on.pop(on.index(n0))] # closed nodes list (F)
+    r = n0
     t = 0
-    while len(on)>0: 
-        index = costs0.index(min(costs0))
-        arc0 = arcs0.pop(index)
-        costs0.remove(costs0[index])
-        arcs += [arc0]
-        print(arc0)
-        if not all([i in cn for i in arc0]):
-            cn += [on.pop(on.index(i)) for i in filter(lambda n : n not in cn, arc0)] # closed nodes list (F)
-        else:
-            pass # otherwise it would close a cycle
+    while len(on)>0:
+        r0 = r
+        narcs = filter(lambda n : n[0] not in cn or n[1] not in cn,\
+        [i for n in cn for j in [[[n,c] for c in network.nodes[n].children],\
+        [[p,n] for p in network.nodes[n].parents]] for i in j])
+        try:        
+            z = min(zip([network.costs[network.arcs.index(i)] for i in narcs],narcs))
+        except:
+            raise Exception('Graph is disconnected.')
+        arcs += [z[1]]
+        r = filter(lambda n : n not in cn, z[1])[0]
+        cn += [on.pop(on.index(r))]   
         t += 1
         if verbose:
             print('Iteration #{}'.format(t))
+            print('r0: {}'.format(r0))
+            print('r: {}'.format(r))
             print('S: {}'.format(arcs))
             print('T: {}'.format(cn))
             print('V: {}'.format(on))
