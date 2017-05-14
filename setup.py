40138f3aa4fed7fe5d3e8ad6a082569e7bb38638
Gabriel S. Gusmão <gusmaogabriels@gmail.com>
2017-05-13 22:37:28 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+
+requires = ['numpy']
+
+packages = [
+		'optinpy',
+		'optinpy.graph',
+		'optinpy.finite_diff',
+		'optinpy.linesearch',
+    'optinpy.linesearch',
+    'optinpy.mcfp',
+    'optinpy.mst',
+    'optinpy.nonlinear',
+    'optinpy.simplex',
+    'optinpy.sp',
+]
+
+package_dir = {'optinpy' : 'optinpy'}
+package_data = { 'optinpy' : []}
+
+
+setup(
+    name='optinpy',
+    version='1.0.0',
+    packages=packages,
+    license='The  GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 License',
+    author = 'Gabriel S. Gusmao',
+    author_email = 'gusmaogabriels@gmail.com',
+    url = 'https://github.com/gusmaogabriels/optinpy',
+    download_url = 'https://github.com/gusmaogabriels/xl2py/tarball/v1.0.0',
+    keywords = ['python', 'optimization', 'linear', 'nonlinear', 'graph', 'node', 'arc'],
+    package_data = package_data,
+    package_dir = package_dir,
+
+)
